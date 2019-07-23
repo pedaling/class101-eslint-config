5b6536c4d4386b17f40818db811cc17e095c0c0a
Youngchan Je <ochanje210@gmail.com>
2019-07-23 14:54:57 +0900

    go eslint! 🏋️🏋️

---
+++ index.js
@@ -0,0 +1,49 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "prettier/react"
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint", "react-hooks", "prettier", "react"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    "array-type": [true, "array"],
+    "interface-name": [true, "never-prefix"],
+    "variable-name": [
+      true,
+      "ban-keywords",
+      "check-format",
+      "allow-pascal-case"
+    ],
+    "no-namespace": false,
+    "object-literal-sort-keys": false,
+    "ordered-imports": true,
+    "no-unused-variable": true,
+    "no-unused-expression": true,
+    quotemark: [true, "single", "jsx-double"],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
+    "prettier/prettier": "error"
+  }
+};
